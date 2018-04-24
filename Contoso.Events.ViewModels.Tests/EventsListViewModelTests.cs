9a0079152da27ad07dc6b257a2e52634dd59e2bf
M Cywka <mcywka@outlook.com>
2018-04-24 12:28:02 +0000

    tests

---
+++ Contoso.Events.ViewModels.Tests/EventsListViewModelTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Contoso.Events.ViewModels.Tests
+{
+    [TestFixture]
+    public class EventsListViewModelTests
+    {
+        public void ShouldPass()
+        {
+            Assert.Pass();
+        }
+    }
+}
